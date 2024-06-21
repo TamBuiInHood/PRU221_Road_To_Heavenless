3d4f2fe55f325e9fea30175668a67ebaa08838a0
Your Name <tungnbse171853@fpt.edu.vn>
2024-06-21 10:43:47 +0700

    fix

--- Assets/Scripts/Scene/PlayAgain.cs
+++ Assets/Scripts/Scene/PlayAgain.cs
@@ -5,10 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class PlayAgain : MonoBehaviour
 {
-    // This method will be called when the button is clicked
     public void OnButtonClick(string sceneName)
     {
-        // Load the scene with the given name
         SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
